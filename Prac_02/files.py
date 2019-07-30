e14f181cf2d423a89f61b45c8634bcee84a42a1e
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2019-07-30 22:09:12 +0800

    Files Exercise: Done part 1

---
+++ Prac_02/files.py
@@ -0,0 +1,10 @@
+"""Do from Scratch Exercise: Files"""
+
+#Exercise 1:
+file_name_out =  open("name.txt", 'w')
+name = input("What is your name? ")
+print(name, file=file_name_out)
+file_name_out.close()
+
+#Excercise 2:
+
