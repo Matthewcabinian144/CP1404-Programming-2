6aff7661395354da8394108743e461cf459872fb
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2019-07-30 22:54:41 +0800

    Added Files exercise 2 and 3, Added numbers.txt

--- Prac_02/files.py
+++ Prac_02/files.py
@@ -7,4 +7,16 @@ print(name, file=file_name_out)
 file_name_out.close()
 
 #Excercise 2:
+file_name_in = open("name.txt", 'r')
+name = file_name_in.read().strip()
+file_name_in.close()
+print("Your name is", name)
 
+#Excercise 3:
+file_numbers = open("numbers.txt", 'r')
+number1 = int(file_numbers.readline())
+number2 = int(file_numbers.readline())
+print(number1 + number2)
+file_numbers.close()
+
+#Excercise 4:
