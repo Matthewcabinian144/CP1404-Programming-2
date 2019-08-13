2a9dbfca2b0cae89c9256c2030186a69bbbdfbd3
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2019-08-13 19:01:30 +0800

    Added Score status

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -6,7 +6,7 @@ elif score >= 90:
 elif score >= 50:
     print("Passable")
 else:
-    print("Bad")99
+    print("Bad")
 
 #version 1
 
@@ -17,3 +17,14 @@ def main():
     print(score_status(score))
 
 
+def score_status(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
