f32a09fc88198c25b5d00c5c789d434bbb4194a7
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2019-08-14 12:17:16 +0800

    Added get_password

--- Prac_03/password_checker.py
+++ Prac_03/password_checker.py
@@ -1,10 +1,16 @@
+MINIMUM_LENGTH = 4
+
 def main():
-
-    
-
-
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisk(password)
 
 
+def get_password()
+    password = input("Enter your password minimum of {} characters: ".format(MINIMUM_LENGTH))
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid: Password Too Short")
+        password = input("Enter your password minimum of {} characters: ".format(MINIMUM_LENGTH))
+    return password
 
 
 
