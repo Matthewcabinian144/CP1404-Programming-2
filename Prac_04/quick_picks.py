9d85b38269463b9ff63fe7ad05a432585a7d715d
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2019-08-20 17:36:02 +0800

    added input and print for quick picks main

--- Prac_04/quick_picks.py
+++ Prac_04/quick_picks.py
@@ -3,3 +3,15 @@ import random
 NUMBERS_PER_LINE = 6
 MINIMUM = 1
 MAXIMUM = 45
+
+def main():
+    amount_of_quick_picks = int(input("How many quick picks? "))
+    while amount_of_quick_picks < 0:
+        print("Invalid, doesn't make sense")
+        amount_of_quick_picks = int(input("How many quick picks? "))
+
+
+
+
+
+main()
