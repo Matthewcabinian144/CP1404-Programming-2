cd0dfe8eb679993b0b108e7773e3ff9c3f8f4ec8
Matthew Cabinian <31027708+Matthewcabinian144@users.noreply.github.com>
2019-09-12 23:12:39 +0800

    Commit convert_m_km.kv and py

--- Prac_07/convert_m_to_km.py
+++ Prac_07/convert_m_to_km.py
@@ -7,27 +7,34 @@ MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
+    output_km = StringProperty()
+
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
 
-    def handle_calculate(self):
-        value = self.get_validated_miles()
-        result = value * MILES_TO_KM
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, text):
+        print("handle calc")
+        miles = self.convert_to_number(text)
+        self.update_result(miles)
 
-    def handle_increment(self, change):
-        value = self.get_validated_miles() + change
-        self.root.ids.input_miles.text = str(value)
-        self.handle_calculate()
+    def handle_increment(self, text, change):
+        print("handle inc")
+        miles = self.convert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
 
-    def get_validated_miles(self):
+    def update_result(self, miles):
+        print("update")
+        self.output_km = str(miles * MILES_TO_KM)
+
+    @staticmethod
+    def convert_to_number(text):
         try:
-            value = float(self.root.ids.input_miles.text)
+            value = float(text)
             return value
         except ValueError:
-            return 0
+            return 0.0
 
 
 MilesConverterApp().run()
